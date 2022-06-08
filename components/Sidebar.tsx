591701d24626d2aba825df19ec35bb8dfda5017f
Md. Rony <rony15-10527@diu.edu.bd>
2022-06-09 03:17:02 +0600

    sidebar updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import Image from "next/image";
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
+    <>
+      <Image
+        src="https://i.ibb.co/6Rjt92g/MdRony.jpg"
+        alt="avatar"
+        className=" mx-auto border rounded-full "
+        height="150px"
+        width="150px"
+        layout="intrinsic"
+        quality="100"
+      />
+      <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
+        <span className="text-green ">Md.</span> Rony
+      </h3>
+    </>
+  );
 }
 
 export default Sidebar
---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="bg-fixed bg-slate-100 dark:from-dark-500 dark:to-dark-700 dark:text-white">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
