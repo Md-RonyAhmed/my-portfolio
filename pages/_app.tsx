cffeb3d6df5051a7ff88a457d53978bc4afd60f5
Md. Rony <rony15-10527@diu.edu.bd>
2022-06-09 02:31:15 +0600

    sidebar and navbar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import Sidebar from '../components/Sidebar';
 import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
+    <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-3 lg:px-3 xl:px-4 ">
       {/* // do this div style later (after putting the content) */}
-      <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
+      <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 shadow-custom-light dark:shadow-custom-dark rounded">
         {/* //!sidebar */}
         <Sidebar />
       </div>
-      <div className="flex flex-col col-span-12 overflow-hidden bg-white shadow-custom-light dark:shadow-custom-dark rounded-2xl lg:col-span-9 dark:bg-dark-500">
+      <div className="flex flex-col col-span-12 overflow-hidden bg-white shadow-custom-light dark:shadow-custom-dark rounded lg:col-span-9 dark:bg-dark-500">
         {/* //!navbar */}
         <Navbar />
         {/* //!about */}
