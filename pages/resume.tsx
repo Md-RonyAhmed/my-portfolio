d715e8c0fc8c59f14b6da9f14a05bd8baf5130bd
Md. Rony <rony15-10527@diu.edu.bd>
2022-06-09 11:06:46 +0600

    Resume sectoin completed

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -12,18 +12,30 @@ const Resume = () => {
             <h5 className="my-2 text-xl font-bold">
               Computer Science Engineering
             </h5>
-            <p className="font-semibold">Daffodil International University (2018-2022)</p>
+            <p className="font-semibold">
+              Daffodil International University (2018-2022)
+            </p>
             <p className="my-3">
-              I have completed my Bsc in Computer Science Engineering with cgpa 3.74 out of 4.00 
+              I have completed my Bsc in Computer Science Engineering with cgpa
+              3.74 out of 4.00
             </p>
           </div>
         </div>
         <div>
           <h5 className="my-3 text-2xl font-bold">Experience</h5>
           <div className="">
-            <h5 className="my-2 text-xl font-bold">Software Developer Jr.</h5>
-            <p className="font-semibold">Tata Consultancy Services</p>
-            <p className="my-3">I don't know why I am doing this job</p>
+            <h5 className="my-2 text-xl font-bold">
+              Software Engineer (Frontend)
+            </h5>
+            <p className="font-semibold">
+              Data Transformers Inc. (10/2021 - Present)
+            </p>
+            <p className="mt-2">Post Oak Blvd Suite, Houston, USA.</p>
+            <p className="mt-2">
+              Email: info@data-transformers.com || Phone: +1 (832) 269-4474 ||
+              Website:{" "}
+              <a className="link" href="https://data-transformers.com">data-transformers.com</a>
+            </p>
           </div>
         </div>
       </div>
@@ -56,7 +68,7 @@ const Resume = () => {
           </div>
         </div>
         <div>
-          <h5 className="my-3 mt-0 text-2xl font-bold">Deployed</h5>
+          <h5 className="my-3 mt-0 text-2xl font-bold">Deployment</h5>
           <div className="my-2 pb-4">
             {hosting.map((tool, i) => (
               <Bar value={tool} key={i} />
