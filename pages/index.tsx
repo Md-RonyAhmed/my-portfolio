ed7fbcc84f63c227a9098e3e5c474803c5c80643
Md. Rony <rony15-10527@diu.edu.bd>
2022-06-09 10:10:26 +0600

    About rout created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,47 @@
-import Head from 'next/head';
-import React from 'react';
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetStaticProps,
+  GetStaticPropsContext,
+  NextPage,
+} from "next";
+import ServiceCard from "../components/ServiceCard";
+import { services } from "../data";
+import { Service } from "../types";
 
-const Home = () => {
+const About: NextPage = () => {
+  // console.log(services);
+
   return (
-    <div>
-      <Head>
-        <title>Md.Rony | Home</title>
-     </Head>
+    <div className="flex flex-col flex-grow px-6 pt-1 ">
+      <h6 className="my-3 text-base font-medium">
+        I am currently pursuing B.Tech Degree(Final Year) in Computer Science
+        Engineering from Academy of Technology. I have 3+ years of experience in
+        Web Development and I have a Youtube Channel where I teach Full Stack
+        Web Development
+      </h6>
+      <div
+        className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h4 className="my-3 text-xl font-semibold tracking-wide">
+          What I am doing
+        </h4>
+
+        <div className="grid gap-6 my-3 md:grid-cols-2">
+          {/* children's initial and animate property should be same as the parent during a stagger effect  */}
+          {services.map((service) => (
+            <div
+              className="col-span-2 p-2 bg-gray-200 rounded-lg dark:bg-dark-200 md:col-span-1 "
+              key={service.title}
+            >
+              <ServiceCard service={service} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default About;
