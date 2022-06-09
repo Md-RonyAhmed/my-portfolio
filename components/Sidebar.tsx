77c158b710fad45fa8b824000eed4d0a8aaa37d9
Md. Rony <rony15-10527@diu.edu.bd>
2022-06-09 09:13:05 +0600

    sidebar completed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,15 +12,17 @@ import Link from 'next/link';
 const Sidebar = () => {
   return (
     <>
-      <Image
-        src="https://i.ibb.co/6Rjt92g/MdRony.jpg"
-        alt="avatar"
-        className=" mx-auto border-2 border-white rounded-full "
-        height="150px"
-        width="150px"
-        layout="intrinsic"
-        quality="100"
-      />
+      <div className="border-2 border-slate-800 dark:border-white rounded-full h-36 w-36 mx-auto">
+        <Image
+          src="https://i.ibb.co/27rZktH/IMG-1636480719952-01.jpg"
+          alt="avatar"
+          className=" mx-auto rounded-full justify-center items-center"
+          height="150"
+          width="150"
+          layout="intrinsic"
+          quality="100"
+        />
+      </div>
       <h3 className="my-4 mb-0 text-3xl font-medium tracking-wider font-kaushan">
         Md. <span className="text-green">Rony</span>
       </h3>
@@ -31,7 +33,7 @@ const Sidebar = () => {
       <a
         target="_blank"
         href="https://drive.google.com/file/d/1KysdX1HeqMmqRilmnUu85jik4FmfFJeo/view?usp=sharing"
-        className="flex items-center justify-center px-2 py-1 my-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-l from-green to-blue-500 focus:outline-none dark:bg-dark-200 dark:bg-black-500"
+        className="flex items-center justify-center px-2 py-2 my-2 text-white bg-black rounded-full w-3/5 mx-auto md:w-8/12 cursor-pointer bg-gradient-to-l from-green to-blue-500 focus:outline-none dark:bg-dark-200 dark:bg-black-500"
       >
         <BsFileEarmarkCheckFill className="w-4 h-4 mr-1 text-gray-600" />
         <span>Check Resume</span>
@@ -61,11 +63,11 @@ const Sidebar = () => {
         className="py-4 my-5 bg-slate-300 dark:bg-dark-200"
         style={{ marginLeft: "-1rem", marginRight: "-1rem" }}
       >
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mb-1">
           <GoLocation className="mr-1" />{" "}
           <span>Mirpur-2,Dhaka,Bangladesh </span>
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mb-1">
           <GoMail className="mr-1" /> <span>mdronyb313@gmail.com </span>
         </div>
         <div className="flex items-center justify-center">
@@ -79,7 +81,7 @@ const Sidebar = () => {
       <button className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none">
         <Link href="/contact">Contact me</Link>
       </button>
-      <button className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-l from-green to-blue-500 focus:outline-none hover:scale-105 ">
+      <button className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-l from-green to-blue-500 focus:outline-none hover:scale-105 transition-all delay-75">
         {/* //TODO remove bg black */}
         Hire me
       </button>
