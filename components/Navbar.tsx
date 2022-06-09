75bbbb0a1dafb99e8568a600cc0decd5859493c1
Md. Rony <rony15-10527@diu.edu.bd>
2022-06-09 09:45:39 +0600

    add nested route

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { useState, useEffect, FunctionComponent } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const NavItem: FunctionComponent<{
+  active: string;
+  setActive: Function;
+  name: string;
+  route: string;
+}> = ({ active, setActive, name, route }) => {
+  return active !== name ? (
+    <Link href={route}>
+      <a>
+        <span
+          className="mx-2 cursor-pointer hover:border-b-4 hover:border-b-blue-500 hover:text-blue-600"
+          onClick={() => setActive(name)}
+        >
+          {name}
+        </span>
+      </a>
+    </Link>
+  ) : null;
+};
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const { pathname } = useRouter();
 
-export default Navbar
+  const [active, setActive] = useState("");
+
+  //later
+  useEffect(() => {
+    if (pathname === "/") setActive("About");
+    else if (pathname === "/projects") setActive("Projects");
+    else if (pathname === "/resume") setActive("Resume");
+    else if (pathname === "/contact") setActive("Contact");
+  }, []);
+
+  return (
+    <div className="flex items-center justify-between px-5 py-3 my-3">
+      <span className="text-xl font-bold border-b-4 md:text-2xl border-green-400">
+        {active}
+      </span>
+
+      <div className="text-base font-normal md:text-xl">
+        <NavItem active={active} setActive={setActive} name="About" route="/" />
+        <NavItem
+          active={active}
+          setActive={setActive}
+          name="Resume"
+          route="/resume"
+        />
+        <NavItem
+          active={active}
+          setActive={setActive}
+          name="Projects"
+          route="/projects"
+        />
+        <NavItem
+          active={active}
+          setActive={setActive}
+          name="Contact"
+          route="/contact"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
